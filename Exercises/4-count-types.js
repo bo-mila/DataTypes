30cf3f5df20c17c3f40d0f5bdab83ced802017b6
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-07-20 11:42:20 +0300

    done

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  const typeCount = {};
+  for (const elem of arr) {
+    const type = typeof elem;
+    const t = typeCount[type] || 0;
+    typeCount[type] = t + 1;
+  }
+  return typeCount;
+};
 
 module.exports = { countTypesInArray };
